bebb3eb1d9495e51c2adf4696915520ba72d99af
+Isaac Shuman <shumani@webdev.gat.com>
2024-08-05 14:58:08 -0700

    added but not tested slurm functionality

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
- ./stable_top_parse_prog/write_metrics & ./node_exporter-1.8.2.linux-amd64/node_exporter --collector.disable-defaults --collector.textfile --collector.textfile.directory=/tmp/added_by_exporter.prom --web.listen-address=:9201 --web.disable-exporter-metrics &
-
+Command I use for starting the pseudo_exporter
 ./stable_top_parse_prog/write_metrics & ./node_exporter-1.8.2.linux-amd64/node_exporter --collector.disable-defaults --collector.textfile --collector.textfile.directory=/tmp/ --web.listen-address=:9201 --web.disable-exporter-metrics &
 
+Possible optimizations:
+    Making scraper pull-based by making it an actual exporter by incorporating Prometheus Cllient libraries
+    Never closing top.
+    Eradicating the extraneous metrics gatthered by node exporter
 
-unit file stuff:
+If the grafana_prom service keeps breaking I should add TimeOut to ensure podman-compose down has time to run.
 
-If it keeps breaking I should add TimeOut to ensure podman-compose down has time to run.
-WantedBy=multi-user.target
-
-
-Location of unit files:/etc/systemd/system 
-
+Bash command for killing processes listenting on ports
 kill $(lsof -i | cut -d ' ' -f 2 | tr '\n' ' ' | cut -d ' ' -f 9-)
--- write_metrics.c
+++ write_metrics.c
@@ -22,26 +22,36 @@
 #define EXPOSITION_FILENAME "/tmp/added_by_pseudo_exporter.prom"
 
 //#define DEBUG
+#define GATHER_TOP
+//#define GATHER_SLURM
+
+#define NUM_OF_TOP_METRICS 3
+#define NUM_OF_SLURM_METRICS 2
 
 int find_empty_line(FILE *fp, char *line, int line_size);
 int is_line_empty(const char *line);
 int find_cols_of_fields(char **fields, int size_of_fields, int *col_nums, FILE *fp, char *line, int line_size);
-int parse_for_metrics(FILE *fp, char *line, int line_size, int *col_nums);
+int parse_top_for_metrics(FILE *fp, char *line, int line_size, int *col_nums);
+void write_top_metrics(void);
+int parse_slurm_for_metricss(FILE *fp, char *line, int line_size, int *col_nums);
+void write_slurm_metrics(void);
+
 struct user_attributes *init_user_atts(char *username);
 void clear_table(void);
-void write_metrics();
 void handle_sigint(int sig);
 
 struct user_attributes {
   char user[MAX_USER_SIZE];
   double cpu_usage;
   double ram_usage;
+  int ncpus;
   UT_hash_handle hh; //makes this structure hashable
 };
 
 struct user_attributes *hash_table = NULL;
 
-FILE *fp;
+FILE *fp; //change to top_fp
+FILE *slurm_fp;
 
 int main(int argc, char **argv) {
     char line[MAX_LINE_SIZE];
@@ -49,28 +59,50 @@ int main(int argc, char **argv) {
     long buffer_size;
     long read_size;
     char *fields[] = {"USER", "%CPU", "%MEM"};
-    int col_nums[3];
+    int col_nums[NUM_OF_TOP_METRICS];
+    char *slurm_fields[] = {"User", "NCPUS"};
+    int slurm_col_nums[NUM_OF_SLURM_METRICS];
 
     signal(SIGINT, handle_sigint);
 
     while(1) {
-      // Open the command for reading
-      fp = popen("top -b -n 1 -w " STRINGIZE_WRAP(TOP_WIDTH), "r"); //assumes 512 is sufficient width
       //system("top -b -n 1 > added_by_exporter.prom");
       //fp = fopen("./added_by_exporter.prom", "r");
+
+      #ifdef GATHER_TOP
+      // Open the command for reading
+      fp = popen("top -b -n 1 -w " STRINGIZE_WRAP(TOP_WIDTH), "r"); //assumes 512 is sufficient width
       if (fp == NULL) {
         printf("Failed to run command\n" );
         handle_sigint(0);
         return 1;
       }
       find_empty_line(fp, line, sizeof(line));
-      find_cols_of_fields((char **) fields, sizeof(fields)/sizeof(fields[0]), col_nums, fp, line, sizeof(line));
-      parse_for_metrics(fp, line, sizeof(line), col_nums);
-      write_metrics(fp);
+      find_cols_of_fields((char **) fields, sizeof(fields)/sizeof(fields[0]), col_nums, fp, line, sizeof(line)); //make sizeof stuff a macro??
+      parse_top_for_metrics(fp, line, sizeof(line), col_nums);
+      write_top_metrics();
+      #endif
+
+      #ifdef GATHER_SLURM
+      slurm_fp = popen("sacct -r gpus  --allusers --format=User,ncpus -X -s R", "r"); //should fail silently
+      find_cols_of_fields((char **) slurm_fields, sizeof(slurm_fields)/sizeof(slurm_fields[0]), slurm_col_nums, slurm_fp, line, sizeof(line));
+      if (fgets(line, sizeof(line), slurm_fp) == NULL) //skip line
+            return 1;
+      parse_slurm_for_metrics(slurm_fp, line, sizeof(line), slurm_col_nums);
+      write_slurm_metrics();
+      #endif
 
       sleep(DELAY);
-      fclose(fp); //the latency between this and popen allows for a potential double free
+
+      //double free if CTRL_C is hit during this block
       clear_table();
+      //find_empty_line(fp, line, sizeof(line));
+      #ifdef GATHER_TOP
+      fclose(fp);
+      #endif
+      #ifdef GATHER_SLURM
+      fclose(slurm_fp)
+      #endif
     }
     return 0; 
 
@@ -126,7 +158,7 @@ int find_cols_of_fields(char **fields, int size_of_fields, int *col_nums, FILE *
   return 0;
 }
 
-int parse_for_metrics(FILE *fp, char *line, int line_size, int *col_nums) {
+int parse_top_for_metrics(FILE *fp, char *line, int line_size, int *col_nums) {
   char *token = token;
   int c = 0;
   struct user_attributes *new_user_atts;
@@ -165,6 +197,38 @@ int parse_for_metrics(FILE *fp, char *line, int line_size, int *col_nums) {
   return 0;
 }
 
+int parse_slurm_for_metrics(FILE *fp, char *line, int line_size, int *col_nums) {
+  char *token = token;
+  int c = 0;
+  struct user_attributes *new_user_atts;
+  
+  while (fgets(line, line_size, fp) != NULL) {
+    c = 0;
+    token = strtok(line, " ");
+    while (token != NULL) {
+      if (c == col_nums[0]) {
+        #ifdef DEBUG
+        printf("%s ", token);
+        #endif
+        HASH_FIND_STR(hash_table, token, new_user_atts);
+        if (new_user_atts == NULL) {
+          new_user_atts = init_user_atts(token);
+          HASH_ADD_STR(hash_table, user, new_user_atts);
+        }
+      }
+      else if (c == col_nums[1]) {
+        #ifdef DEBUG
+        printf("%s ", token);
+        #endif
+        new_user_atts->ncpus = new_user_atts->ncpus + atoi(token);
+      }
+      token = strtok(NULL, " ");
+      c++;
+    }
+  }
+  return 0;
+}
+
 struct user_attributes *init_user_atts(char *username) {
   struct user_attributes *new_user_atts;
   new_user_atts = malloc(sizeof(struct user_attributes));
@@ -186,7 +250,7 @@ void clear_table(void)
     }
 }
 
-void write_metrics() {
+void write_top_metrics(void) {
   struct user_attributes *current_user;
   struct user_attributes *tmp;
   FILE *file = fopen(EXPOSITION_FILENAME, "w");
@@ -207,9 +271,32 @@ void write_metrics() {
   HASH_ITER(hh, hash_table, current_user, tmp) {
     fprintf(file, "ram_usage{user=\"%s\"} %.2f\n", current_user->user, current_user->ram_usage);
   }
-  
   fclose(file);
 }
+
+void write_slurm_metrics(void) {
+  struct user_attributes *current_user;
+  struct user_attributes *tmp;
+
+  #ifdef GATHER_TOP
+  FILE *file = fopen(EXPOSITION_FILENAME, "a"); //assumes top has been run prior
+  #else
+  FILE *file = fopen(EXPOSITION_FILENAME, "w");
+  #endif
+
+  //add test case for making sure there are no traling spaces
+
+  fprintf(file, "# HELP ncpu how many cpus the user is using on the gpu nodes\n\
+# TYPE ncpu gauge\n");
+  fprintf(file, "cpu_usage{user=\"not_from_top\"} %.2f\n", 4.0);
+  
+  HASH_ITER(hh, hash_table, current_user, tmp) {
+      fprintf(file, "cpu_usage{user=\"%s\"} %.2f\n", current_user->user, current_user->ncpus);
+  }
+
+  fclose(file);
+}
+
 
 void handle_sigint(int sig) {
   printf("\nMetrics writer exiting\n");
