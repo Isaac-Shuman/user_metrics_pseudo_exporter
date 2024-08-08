000e21e0d799ff8a43b8e3792cf870cff8961657
+Isaac Shuman <shumani@webdev.gat.com>
2024-08-08 16:08:51 -0700

    broke slurm functionality because we are aggregating by pgid instead of user

--- write_metrics.c
+++ write_metrics.c
@@ -12,6 +12,7 @@
 #define MAX_LINE_SIZE 256
 #define MAX_FILE_PATH 256
 #define MAX_USER_SIZE 64 //only used for mallocing the user name when storing it in the user_attributes structure
+#define MAX_COMMAND_SIZE 64
 #define DELAY 3
 
 #define TOP_WIDTH 512 //passed with -w flag
@@ -22,16 +23,18 @@
 //#define EXPOSITION_FILENAME "/tmp/added_by_pseudo_exporter.prom"
 
 //#define DEBUG
-#define GATHER_TOP
+//#define GATHER_TOP
 //#define GATHER_SLURM
+#define GATHER_PS
 
 #define NUM_OF_TOP_METRICS 3
 #define NUM_OF_SLURM_METRICS 2
+#define NUM_OF_PS_ATTRIBUTES 6
 
 #define WRITE_METRICS_FLOAT(metric_name, help_msg, type)                                                 \
   do {                                                                                                   \
-    struct user_attributes *current_user;                                                                \
-    struct user_attributes *tmp;                                                                         \
+    struct process_group_attributes *current_user;                                                                \
+    struct process_group_attributes *tmp;                                                                         \
     FILE *file = fopen(EXPOSITION_FILENAME, "a");                                                        \                                       
     fprintf(file, "\n# HELP " #metric_name " " help_msg "\n# TYPE " #metric_name " " type "\n");                      \
     HASH_ITER(hh, hash_table, current_user, tmp) {                                                      \
@@ -40,19 +43,35 @@
     fclose(file);                                                                                        \
   } while (0)       
 
+#define WRITE_METRICS_PG_FLOAT(metric_name, help_msg, type)                                                 \
+  do {                                                                                                   \
+    struct process_group_attributes *current_user;                                                                \
+    struct process_group_attributes *tmp;                                                                         \
+    FILE *file = fopen(EXPOSITION_FILENAME, "a");                                                        \                                       
+    fprintf(file, "\n# HELP " #metric_name " " help_msg "\n# TYPE " #metric_name " " type "\n");                      \
+    HASH_ITER(hh, hash_table, current_user, tmp) {                                                      \
+      fprintf(file, #metric_name"{user=\"%s\",pgid=\"%i\",command=\"%s\"} %.2f\n",  \
+      current_user->user, current_user->pgid, current_user->command, (double) current_user->metric_name);   \
+    }                                                                                                    \
+    fclose(file);                                                                                        \
+  } while (0)       
+
 int find_empty_line(FILE *fp, char *line, int line_size);
 int is_line_empty(const char *line);
 int find_cols_of_fields(char **fields, int size_of_fields, int *col_nums, FILE *fp, char *line, int line_size);
 int parse_top_for_metrics(FILE *fp, char *line, int line_size, int *col_nums);
-void write_top_metrics(void);
 int parse_slurm_for_metrics(FILE *fp, char *line, int line_size, int *col_nums);
-void write_slurm_metrics(void);
+int parse_ps_for_metrics(FILE *fp, char *line, int line_size, int *col_nums);
 
-struct user_attributes *init_user_atts(char *username);
+
+struct process_group_attributes *init_process_group_atts(int pgid);
 void clear_table(void);
 void handle_sigint(int sig);
 
-struct user_attributes {
+
+struct process_group_attributes {
+  int pgid;
+  char command[MAX_COMMAND_SIZE];
   char user[MAX_USER_SIZE];
   double cpu_usage;
   double ram_usage;
@@ -60,10 +79,11 @@ struct user_attributes {
   UT_hash_handle hh; //makes this structure hashable
 };
 
-struct user_attributes *hash_table = NULL;
+struct process_group_attributes *hash_table = NULL;
 
 FILE *fp; //change to top_fp
 FILE *slurm_fp;
+FILE *ps_fp;
 
 int main(int argc, char **argv) {
     char line[MAX_LINE_SIZE];
@@ -74,6 +94,8 @@ int main(int argc, char **argv) {
     int col_nums[NUM_OF_TOP_METRICS];
     char *slurm_fields[] = {"User", "NCPUS"};
     int slurm_col_nums[NUM_OF_SLURM_METRICS];
+    char *ps_fields[] = {"PID", "PGID", "USER", "COMMAND", "%CPU", "%MEM"};
+    int ps_col_nums[NUM_OF_PS_ATTRIBUTES]; 
 
     signal(SIGINT, handle_sigint);
 
@@ -101,8 +123,17 @@ int main(int argc, char **argv) {
       if (fgets(line, sizeof(line), slurm_fp) == NULL) //skip line
             return 1;
       parse_slurm_for_metrics(slurm_fp, line, sizeof(line), slurm_col_nums);
-      WRITE_METRICS_FLOAT(ncpus, "how many cpus the user is using on the gpu nodes", "gauge");
-      //write_slurm_metrics();
+      #endif
+
+      #ifdef GATHER_PS
+      ps_fp = popen("ps -eo pid,pgid,user,comm,pcpu,pmem", "r");
+      if (ps_fp == NULL) {
+        printf("Failed to run command\n" );
+        handle_sigint(0);
+        return 1;
+      }
+      find_cols_of_fields((char **) ps_fields, sizeof(ps_fields)/sizeof(ps_fields[0]), ps_col_nums, ps_fp, line, sizeof(line));
+      parse_ps_for_metrics(ps_fp, line, sizeof(line), ps_col_nums);
       #endif
 
       fclose(fopen(EXPOSITION_FILENAME, "w")); //empty file
@@ -112,6 +143,10 @@ int main(int argc, char **argv) {
       #endif
       #ifdef GATHER_SLURM
       WRITE_METRICS_FLOAT(ncpus, "how many cpus the user is using on the gpu nodes", "gauge");
+      #endif
+      #ifdef GATHER_PS
+      WRITE_METRICS_PG_FLOAT(cpu_usage, "how much cpu a user is using", "gauge");
+      WRITE_METRICS_PG_FLOAT(ram_usage, "how much ram a user is using", "gauge");
       #endif
 
       sleep(DELAY);
@@ -124,6 +159,9 @@ int main(int argc, char **argv) {
       #endif
       #ifdef GATHER_SLURM
       pclose(slurm_fp);
+      #endif
+      #ifdef GATHER_PS
+      pclose(ps_fp);
       #endif
     }
     return 0; 
@@ -180,10 +218,12 @@ int find_cols_of_fields(char **fields, int size_of_fields, int *col_nums, FILE *
   return 0;
 }
 
-int parse_top_for_metrics(FILE *fp, char *line, int line_size, int *col_nums) {
+int parse_ps_for_metrics(FILE *fp, char *line, int line_size, int *col_nums) {
   char *token = NULL;
   int c = 0;
-  struct user_attributes *new_user_atts;
+  int pid_val = 0;
+  int pgid_val = 0;
+  struct process_group_attributes *new_user_atts = NULL;
   
   while (fgets(line, line_size, fp) != NULL) {
     c = 0;
@@ -193,57 +233,48 @@ int parse_top_for_metrics(FILE *fp, char *line, int line_size, int *col_nums) {
         #ifdef DEBUG
         printf("%s ", token);
         #endif
-        HASH_FIND_STR(hash_table, token, new_user_atts);
-        if (new_user_atts == NULL) {
-          new_user_atts = init_user_atts(token);
-          HASH_ADD_STR(hash_table, user, new_user_atts);
-        }
+        pid_val = atoi(token);
       }
       else if (c == col_nums[1]) {
         #ifdef DEBUG
         printf("%s ", token);
         #endif
-        new_user_atts->cpu_usage = new_user_atts->cpu_usage + atof(token);
+        pgid_val = atoi(token);
+        HASH_FIND_INT(hash_table, &pgid_val, new_user_atts);
+        if (new_user_atts == NULL) {
+          new_user_atts = init_process_group_atts(pgid_val);
+          HASH_ADD_INT(hash_table, pgid, new_user_atts);
+        }
       }
       else if (c == col_nums[2]) {
+        #ifdef DEBUG
+        printf("%s \n", token); //only occurs for process group leader
+        #endif
+        if (pid_val == pgid_val) {
+          strcpy(new_user_atts->user, token);
+        }
+      }
+      else if (c == col_nums[3]) {
+        #ifdef DEBUG
+        printf("%s \n", token);
+        #endif
+        if (pid_val == pgid_val) { //only occurs for process group leader
+          strcpy(new_user_atts->command, token);
+        }
+      }
+      else if (c == col_nums[4]) {
+        #ifdef DEBUG
+        printf("%s \n", token);
+        #endif
+        new_user_atts->cpu_usage = new_user_atts->cpu_usage + atof(token);
+      }
+      else if (c == col_nums[5]) {
         #ifdef DEBUG
         printf("%s \n", token);
         #endif
         new_user_atts->ram_usage = new_user_atts->ram_usage + atof(token);
       }
-      token = strtok(NULL, " ");
-      c++;
-    }
-  }
 
-  return 0;
-}
-
-int parse_slurm_for_metrics(FILE *fp, char *line, int line_size, int *col_nums) {
-  char *token = NULL;
-  int c = 0;
-  struct user_attributes *new_user_atts;
-  
-  while (fgets(line, line_size, fp) != NULL) {
-    c = 0;
-    token = strtok(line, " ");
-    while (token != NULL) {
-      if (c == col_nums[0]) {
-        #ifdef DEBUG
-        printf("%s ", token);
-        #endif
-        HASH_FIND_STR(hash_table, token, new_user_atts);
-        if (new_user_atts == NULL) {
-          new_user_atts = init_user_atts(token);
-          HASH_ADD_STR(hash_table, user, new_user_atts);
-        }
-      }
-      else if (c == col_nums[1]) {
-        #ifdef DEBUG
-        printf("%s ", token);
-        #endif
-        new_user_atts->ncpus = new_user_atts->ncpus + atoi(token);
-      }
       token = strtok(NULL, " ");
       c++;
     }
@@ -251,77 +282,28 @@ int parse_slurm_for_metrics(FILE *fp, char *line, int line_size, int *col_nums) 
   return 0;
 }
 
-struct user_attributes *init_user_atts(char *username) {
-  struct user_attributes *new_user_atts;
-  new_user_atts = malloc(sizeof(struct user_attributes));
-  strcpy(new_user_atts->user, username);
+struct process_group_attributes *init_process_group_atts(int pgid) {
+  struct process_group_attributes *new_user_atts;
+  new_user_atts = malloc(sizeof(struct process_group_attributes));
+  //strcpy(new_user_atts->user, username);
   new_user_atts->cpu_usage = 0.0;
   new_user_atts->ram_usage = 0.0;
   new_user_atts->ncpus = 0;
+  new_user_atts->pgid = pgid;
 
   return new_user_atts;
 }
 
 void clear_table(void)
 {
-    struct user_attributes *current_user;
-    struct user_attributes *tmp;
+    struct process_group_attributes *current_user;
+    struct process_group_attributes *tmp;
 
     HASH_ITER(hh, hash_table, current_user, tmp) {
         HASH_DEL(hash_table, current_user);  /* delete it (users advances to next) */
         free(current_user);             /* free it */
     }
 }
-
-/*
-void write_top_metrics(void) {
-  struct user_attributes *current_user;
-  struct user_attributes *tmp;
-  FILE *file = fopen(EXPOSITION_FILENAME, "w");
-
-  //add test case for making sure there are no traling spaces
-
-  fprintf(file, "# HELP cpu_usage how much cpu a user is using\n\
-# TYPE cpu_usage gauge\n");
-  fprintf(file, "cpu_usage{user=\"not_from_top\"} %.2f\n", 4.0);
-  
-  HASH_ITER(hh, hash_table, current_user, tmp) {
-      fprintf(file, "cpu_usage{user=\"%s\"} %.2f\n", current_user->user, current_user->cpu_usage);
-  }
-
-  fprintf(file, "\n# HELP ram_usage how much ram a user is using\n\# TYPE ram_usage gauge\n");
-  fprintf(file, "ram_usage{user=\"not_from_top\"} %.2f\n", 4.0);
-  
-  HASH_ITER(hh, hash_table, current_user, tmp) {
-    fprintf(file, "ram_usage{user=\"%s\"} %.2f\n", current_user->user, current_user->ram_usage);
-  }
-  fclose(file);
-}
-*/                                                                                     
-
-/*
-void write_slurm_metrics(void) {
-  struct user_attributes *current_user;
-  struct user_attributes *tmp;
-
-  #ifdef GATHER_TOP
-  FILE *file = fopen(EXPOSITION_FILENAME, "a"); //assumes top has been run prior
-  #else
-  FILE *file = fopen(EXPOSITION_FILENAME, "w");
-  #endif
-
-  //add test case for making sure there are no traling spaces
-
-  fprintf(file, "\n# HELP ncpu how many cpus the user is using on the gpu nodes\n\
-# TYPE ncpu gauge\n");
-  fprintf(file, "ncpu{user=\"not_from_top\"} %.2f\n", 4.0);
-  
-  HASH_ITER(hh, hash_table, current_user, tmp) {
-      fprintf(file, "ncpu{user=\"%s\"} %.2f\n", current_user->user, (double) current_user->ncpus);
-  }
-  fclose(file);
-}
-*/
 
 
 void handle_sigint(int sig) {
